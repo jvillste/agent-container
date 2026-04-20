4f8f0de40d0bcc640d26168e01e9502a872a5d61
Jukka Villstedt <jukka.villstedt@iki.fi>
2026-04-20 10:29:59 +0300

    add openrouter

--- run.sh
+++ run.sh
@@ -9,10 +9,12 @@ cp "${AGENT_CONTAINERS_HOME}/AGENTS.md" "${AGENT_CONTAINER_HOME}/pi/agent/"
 JUKKA_OPENAI_API_KEY_FILE_NAME="${AGENT_CONTAINER_HOME}/pi/jukka-openai-api-key"
 NITOR_OPENAI_API_KEY_FILE_NAME="${AGENT_CONTAINER_HOME}/pi/nitor-openai-api-key"
 JUKKA_HUGGINGFACE_API_KEY_FILE_NAME="${AGENT_CONTAINER_HOME}/pi/jukka-huggingface-api-key"
+JUKKA_OPENROUTER_API_KEY_FILE_NAME="${AGENT_CONTAINER_HOME}/pi/jukka-openrouter-api-key"
 
 security find-generic-password -s "jukka-openai-api-key" -a "jukka-openai-api-key" -w > "${JUKKA_OPENAI_API_KEY_FILE_NAME}"
 security find-generic-password -s "nitor-openai-api-key" -a "nitor-openai-api-key" -w > "${NITOR_OPENAI_API_KEY_FILE_NAME}"
 security find-generic-password -s "jukka-huggingface-api-key" -a "jukka-huggingface-api-key" -w > "${JUKKA_HUGGINGFACE_API_KEY_FILE_NAME}"
+security find-generic-password -s "jukka-openrouter-api-key" -a "jukka-openrouter-api-key" -w > "${JUKKA_OPENROUTER_API_KEY_FILE_NAME}"
 
 docker run --rm -it \
       --cap-drop=ALL \
@@ -30,3 +32,4 @@ docker run --rm -it \
 rm "${JUKKA_OPENAI_API_KEY_FILE_NAME}"
 rm "${NITOR_OPENAI_API_KEY_FILE_NAME}"
 rm "${JUKKA_HUGGINGFACE_API_KEY_FILE_NAME}"
+rm "${JUKKA_OPENROUTER_API_KEY_FILE_NAME}"
