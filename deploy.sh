641c4dd49f8340f770852e4426dcf209afb4e873
Jukka Villstedt <jukka.villstedt@iki.fi>
2026-05-07 18:20:26 +0300

    add start-shell-in-agent-container

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-cp run-agent-container ~/bin/run-agent-container
-cp remove-agent-container ~/bin/remove-agent-container
+cp run-agent-container ~/bin/
+cp remove-agent-container ~/bin/
+cp start-shell-in-agent-container ~/bin/
 
 rm -rf ~/agent-container-resources
 cp -r resources ~/agent-container-resources
