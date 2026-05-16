5aef1018bb30f43de10331d59afd3fd7ac2e9520
Jukka Villstedt <jukka.villstedt@iki.fi>
2026-05-16 15:06:11 +0300

    rename project-name to container-name

--- clj/agent_container/core.clj
+++ clj/agent_container/core.clj
@@ -10,63 +10,64 @@
 (defn- current-working-directory []
   (System/getProperty "user.dir"))
 
-(defn project-name []
+(defn container-name []
   (basename (current-working-directory)))
 
 (defn run []
   (let [current-working-directory (current-working-directory)
-        project-name (basename current-working-directory)
+        container-name (basename current-working-directory)
         resources-dir (str (System/getenv "HOME") "/agent-container-resources")
         container-exists? (not (empty? (:out (process/shell {:out :string :exit? true}
-                                                            (format "docker ps -a --filter name=^%s$ --format '{{.Names}}'" project-name)))))]
+                                                            (format "docker ps -a --filter name=^%s$ --format '{{.Names}}'" container-name)))))]
     (when-not container-exists?
-      (println "creating container" project-name)
+      (println "creating container" container-name)
       (process/shell {:inherit? true}
                      (format
-                      "docker create --name %s
-            --cap-drop=ALL
-            --cap-add=CHOWN
-            --cap-add=DAC_OVERRIDE
-            --cap-add=FOWNER
-            --cap-add=FSETID
-            --cap-add=SETFCAP
-            --cap-add=SETUID
-            --cap-add=SETGID
-            --security-opt=no-new-privileges
-            --memory=16g
-            --pids-limit=512
-            -e TAVILY_API_KEY=\"$(security find-generic-password -s travily-api-key -a travily-api-key -w)\"
-            -e JUKKA_OPENAI_API_KEY=\"$(security find-generic-password -s jukka-openai-api-key -a jukka-openai-api-key -w)\"
-            -e JUKKA_HUGGINGFACE_API_KEY=\"$(security find-generic-password -s jukka-huggingface-api-key -a jukka-huggingface-api-key -w)\"
-            -e JUKKA_OPENROUTER_API_KEY=\"$(security find-generic-password -s jukka-openrouter-api-key -a jukka-openrouter-api-key -w)\"
-            -v \"%s:/workspace\"
-            -w /workspace
-            agent-container:latest"
-                      project-name
+                      "docker create
+                              --name %s
+                              --cap-drop=ALL
+                              --cap-add=CHOWN
+                              --cap-add=DAC_OVERRIDE
+                              --cap-add=FOWNER
+                              --cap-add=FSETID
+                              --cap-add=SETFCAP
+                              --cap-add=SETUID
+                              --cap-add=SETGID
+                              --security-opt=no-new-privileges
+                              --memory=16g
+                              --pids-limit=512
+                              -e TAVILY_API_KEY=\"$(security find-generic-password -s travily-api-key -a travily-api-key -w)\"
+                              -e JUKKA_OPENAI_API_KEY=\"$(security find-generic-password -s jukka-openai-api-key -a jukka-openai-api-key -w)\"
+                              -e JUKKA_HUGGINGFACE_API_KEY=\"$(security find-generic-password -s jukka-huggingface-api-key -a jukka-huggingface-api-key -w)\"
+                              -e JUKKA_OPENROUTER_API_KEY=\"$(security find-generic-password -s jukka-openrouter-api-key -a jukka-openrouter-api-key -w)\"
+                              -v \"%s:/workspace\"
+                              -w /workspace
+                              agent-container:latest"
+                      container-name
                       current-working-directory))
 
-      (process/shell (format "docker cp %s/settings.json %s:/root/.pi/agent/" resources-dir project-name)))
+      (process/shell (format "docker cp %s/settings.json %s:/root/.pi/agent/" resources-dir container-name)))
 
-    (process/shell (format "docker start %s" project-name))
-    (process/shell (format "docker cp %s/AGENTS.md %s:/root/.pi/agent/" resources-dir project-name))
-    (process/shell (format "docker cp %s/models.json %s:/root/.pi/agent/" resources-dir project-name))
+    (process/shell (format "docker start %s" container-name))
+    (process/shell (format "docker cp %s/AGENTS.md %s:/root/.pi/agent/" resources-dir container-name))
+    (process/shell (format "docker cp %s/models.json %s:/root/.pi/agent/" resources-dir container-name))
     (doseq [skill-dir (.listFiles (File. (str resources-dir "/skills")))]
       (when (.isDirectory skill-dir)
         (process/shell (format "docker cp %s %s:/root/.pi/agent/skills/%s"
                                skill-dir
-                               project-name
+                               container-name
                                (.getName skill-dir)))))
-    (process/shell (format "docker exec %s touch /root/this-is-an-agent-container" project-name))
+    (process/shell (format "docker exec %s touch /root/this-is-an-agent-container" container-name))
 
-    (process/shell (format "docker exec -it --detach-keys=ctrl-z,z %s bash" project-name))
+    (process/shell (format "docker exec -it --detach-keys=ctrl-z,z %s bash" container-name))
 
-    (process/shell (format "docker stop %s" project-name))))
+    (process/shell (format "docker stop %s" container-name))))
 
 (defn remove-container {:command-name "remove"}[]
-  (process/shell (str "docker rm " (project-name))))
+  (process/shell (str "docker rm " (container-name))))
 
 (defn bash []
-  (process/shell (str "docker exec -it --detach-keys='ctrl-z,z' " (project-name) " bash")))
+  (process/shell (str "docker exec -it --detach-keys='ctrl-z,z' " (container-name) " bash")))
 
 (def commands [#'run
                #'remove-container
